770ee7d587e3633f0843d40837f488ff337f4bb5
A-lex-G <alexey803@gmail.com>
2023-03-31 19:05:31 +0300

    task 2 of 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class = "gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title ="${description}"></a></li>`
+);
+
+container.insertAdjacentHTML("afterbegin", markup.join(""));
+
+container.addEventListener("click", onImageClick);
+
+let lightbox = new SimpleLightbox(".gallery a");
+
+function onImageClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  lightbox.on("shown.simplelightbox", function () {
+    lightbox.options.captionDelay = 250;
+  });
+}
